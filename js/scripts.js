1c592b812a4d3c665c68574c0f78b4519cc3da56
meglybbert <meglybbert@eagles.ewu.edu>
2017-05-13 16:11:08 -0700

    Added Wes Bos and my touches

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,36 @@
 // @codekit-prepend "semantic.js";
 
 console.log('Hello, World!');
+
+
+const dogs = [{ name: 'Snickers', age: 2 }, { name: 'hugo', age: 8 }];
+
+function makeGreen() {
+  const p = document.querySelector('p');
+  p.style.color = '#BADA55';
+  p.style.fontSize = '50px';
+}
+
+    // Regular
+
+    // Interpolated
+
+    // Styled
+
+    // warning!
+
+    // Error :|
+
+    // Info
+
+    // Testing
+
+    // clearing
+
+    // Viewing DOM Elements
+
+    // Grouping together
+
+    // counting
+
+    // timing
