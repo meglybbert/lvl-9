a5e77ebed77e8a3d64cdbcb5a9c254d36b7f2c00
meglybbert <meglybbert@eagles.ewu.edu>
2017-05-13 16:16:55 -0700

    ...

--- js/scripts.js
+++ js/scripts.js
@@ -13,26 +13,77 @@ function makeGreen() {
   p.style.boxShadow = '5px';
 }
 
-    // Regular
+    const dogs = [{ name: 'Snickers', age: 2 }, { name: 'hugo', age: 8 }];
 
-    // Interpolated
+    function makeGreen() {
+      const p = document.querySelector('p');
+      p.style.color = '#BADA55';
+      p.style.fontSize = '50px';
+    }
 
-    // Styled
+// Regular
+console.log('hello');
 
-    // warning!
+// Interpolated
+console.log('Hello I am a %s string!', '💩');
 
-    // Error :|
+// Styled
+// console.log('%c I am some great text', 'font-size:50px; background:red; text-shadow: 10px 10px 0 blue')
 
-    // Info
+// warning!
+console.warn('OH NOOO');
 
-    // Testing
+// Error :|
+console.error('Shit!');
 
-    // clearing
+// Info
+console.info('Crocodiles eat 3-4 people per year');
 
-    // Viewing DOM Elements
+// Testing
+const p = document.querySelector('p');
 
-    // Grouping together
+console.assert(p.classList.contains('ouch'), 'That is wrong!');
 
-    // counting
+// clearing
+console.clear();
 
-    // timing
+// Viewing DOM Elements
+console.log(p);
+console.dir(p);
+
+console.clear();
+
+// Grouping together
+dogs.forEach(dog => {
+  console.groupCollapsed(`${dog.name}`);
+  console.log(`This is ${dog.name}`);
+  console.log(`${dog.name} is ${dog.age} years old`);
+  console.log(`${dog.name} is ${dog.age * 7} dog years old`);
+  console.groupEnd(`${dog.name}`);
+});
+
+// counting
+
+console.count('Wes');
+console.count('Wes');
+console.count('Steve');
+console.count('Steve');
+console.count('Wes');
+console.count('Steve');
+console.count('Wes');
+console.count('Steve');
+console.count('Steve');
+console.count('Steve');
+console.count('Steve');
+console.count('Steve');
+
+// timing
+console.time('fetching data');
+fetch('https://api.github.com/users/wesbos')
+  .then(data => data.json())
+  .then(data => {
+    console.timeEnd('fetching data');
+    console.log(data);
+  });
+
+console.table(dogs);
