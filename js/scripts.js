f8da41997ac2c8fb817ad0a41c3272e924564f60
meglybbert <meglybbert@eagles.ewu.edu>
2017-05-13 16:14:20 -0700

    Rebuilt for pages

--- js/scripts.js
+++ js/scripts.js
@@ -8,8 +8,9 @@ const dogs = [{ name: 'Snickers', age: 2 }, { name: 'hugo', age: 8 }];
 
 function makeGreen() {
   const p = document.querySelector('p');
-  p.style.color = '#BADA55';
-  p.style.fontSize = '50px';
+  p.style.color = '#96c10e';
+  p.style.fontSize = '3em';
+  p.style.boxShadow = '5px';
 }
 
     // Regular
